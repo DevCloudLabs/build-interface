2e1ba31676c9b8fc4402c7eb1d13e9216a792c27
= <=>
2023-11-30 11:14:08 +0530

    configure content

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { MdOutlineBook } from "react-icons/md";
+
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl"><MdOutlineBook />Your Appointments</h1>
+      <h1 className="text-5xl"><MdOutlineBook className="inline-block" text-red-200 />Your Appointments</h1>
+      <p>This is so awesome</p>
     </div>
   );
 }
