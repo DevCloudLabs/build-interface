657014cce1269f59b839a991f018e359a98620fb
= <=>
2023-11-30 10:17:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { MdOutlineBook } from "react-icons/md";
 function App() {
   return (
-    <div className="App">
-      <h1><MdOutlineBook />Your Appointments</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl"><MdOutlineBook />Your Appointments</h1>
     </div>
   );
 }
