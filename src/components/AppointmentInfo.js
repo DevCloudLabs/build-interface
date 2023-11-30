a34da03430b3565ad78190933293b87381171107
= <=>
2023-11-30 13:41:57 +0530

    Update AppointmentInfo.js

--- src/components/AppointmentInfo.js
+++ src/components/AppointmentInfo.js
@@ -1,6 +1,6 @@
 import { BiTrash } from "react-icons/bi";
 
-function AppointmentList({appointment}) {
+function AppointmentList({ appointment }) {
   return (
     <li className="px-3 py-3 flex items-start">
       <button
